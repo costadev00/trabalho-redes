c260bff3dd1ba85c10799e0501e4df07449b633c
Matheus Costa Monteiro <matheuscostamonteiro.mc@gmail.com>
2023-02-06 12:00:36 -0300

    oh my gosh what a semester

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Digite seu usuário e senha: ";
+    string usuario, senha;
+    cin>>usuario>>senha;
+    //sign a user and password, store the data on the variables and output that in txt file
+    
+    return 0;
+}
